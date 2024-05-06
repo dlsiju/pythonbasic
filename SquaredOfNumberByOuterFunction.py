5d56a21a86491279981819a709f5c2d4771ebf68
Siju Xminds <sijudl@xminds.com>
2024-05-06 15:04:26 +0530

    Code clean up

--- SquaredOfNumberByOuterFunction.py
+++ SquaredOfNumberByOuterFunction.py
@@ -1,8 +1,8 @@
-def squaredOfNumbers(numb1, num2):
+def squaredOfNumbers(num1, num2):
     def squareOfSecondNumber(num2):
         return num2 * num2
 
-    return squareOfSecondNumber(num2) + (numb1 * numb1)
+    return squareOfSecondNumber(num1) + squareOfSecondNumber(num2)
 
 
 number1 = int(input("Enter first number: "))
