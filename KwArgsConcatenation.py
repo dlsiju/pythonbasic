d762e3967550874638df57550f66c847fea828d6
Siju Xminds <sijudl@xminds.com>
2024-05-06 13:59:06 +0530

    append all string read by console

---
+++ KwArgsConcatenation.py
@@ -0,0 +1,15 @@
+def keyWordArgsConcatenation(**keykwds):
+    appendedstring = ''
+    for key in keykwds.keys():
+        appendedstring += keykwds[key]
+    return appendedstring
+
+
+print('Enter four string values to concatenate using keyword arguments:')
+firstString = input('First string: ')
+secondString = input('Second string: ')
+thirdString = input('Third string: ')
+fourthString = input('Fourth string: ')
+appendedStringVlaue = keyWordArgsConcatenation(third=thirdString, second=secondString, fourth=fourthString,
+                                               first=firstString)
+print("Appended String:", appendedStringVlaue)
