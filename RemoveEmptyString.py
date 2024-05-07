b51cee89faac8e8ca9e4c4b6923a013b8742d231
Siju Xminds <sijudl@xminds.com>
2024-05-07 17:28:38 +0530

    Remove all empty string from a list of string

---
+++ RemoveEmptyString.py
@@ -0,0 +1,8 @@
+listSize = int(input("Enter size of list: "))
+listOfKeyword = []
+for i in range(0, listSize):
+    listOfKeyword.append(input('Enter keyword: '))
+print('list of keyword=', listOfKeyword)
+while ('' in listOfKeyword):
+    listOfKeyword.remove('')
+print('list of keywords after removal of empty space=', listOfKeyword)
