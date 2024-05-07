f2a8089d27ca94a1fadc2a5e410d61480c0deaba
Siju Xminds <sijudl@xminds.com>
2024-05-07 16:36:12 +0530

    convert input strings to lower case

--- FindOccurance.py
+++ FindOccurance.py
@@ -1,12 +1,11 @@
 inputString = input("Enter a string: ")
-print('inputString=', inputString)
 searchKey = input('Enter a search key')
-print('searchKey=', searchKey)
 index = 0
 indexList = []
 occurrenceCount = 0
+inputString = inputString.casefold()
 while index < len(inputString):
-    if inputString.startswith(searchKey, index):
+    if inputString.startswith(searchKey.casefold(), index):
         occurrenceCount += 1
         indexList.append(index)
     index += 1
