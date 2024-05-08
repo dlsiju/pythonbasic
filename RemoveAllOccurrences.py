4eec90576b68543ece14cb5b4ca4599adeafa97c
Siju Xminds <sijudl@xminds.com>
2024-05-08 19:00:09 +0530

    Convert list to dictionary

--- RemoveAllOccurrences.py
+++ RemoveAllOccurrences.py
@@ -1,8 +1,8 @@
 # 3.Remove all occurrences of a specific item from a list.
 
-list1 = ['red', 'green', 'blue', 'red', 'green', 'indigo', 'yellow', 'red', 'violet', 'white', 'black', 'orange']
-print('list1 =', list1)
+colorList = ['red', 'green', 'blue', 'red', 'green', 'indigo', 'yellow', 'red', 'violet', 'white', 'black', 'orange']
+print('list1 =', colorList)
 itemTobeRemoved = input('enter any item to remove if it is present')
 print('item to remove =', itemTobeRemoved)
-list1 = [item for item in list1 if item != itemTobeRemoved]
-print('list1 after removal =', list1)
+colorList = [item for item in colorList if item != itemTobeRemoved]
+print('list1 after removal =', colorList)
