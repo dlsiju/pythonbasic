98141e48f2e797ba8d7d0bdecf79a95397dc8cf8
Siju Xminds <sijudl@xminds.com>
2024-05-08 16:39:04 +0530

    Print the first 2 elements of sublist at position 3

--- PrintFirstTwoElementsOfSublist.py
+++ PrintFirstTwoElementsOfSublist.py
@@ -1,5 +1,5 @@
 # 6.Given a list [10,23,40,[9,3,4],6,7].Print the first 2 elements of sublist at position 3
 
 list = [10, 23, 40, [9, 3, 4], 6, 7]
-print('print using index')
+print('the first 2 elements of sublist at position 3 using index are,')
 print(list[3][0:2])
