40e157c6a291d7e7d070fba1e21f66273e8af11d
Siju Xminds <sijudl@xminds.com>
2024-05-08 16:00:18 +0530

    Iterate multiple list simultaneously using zip function

---
+++ ZipFunction.py
@@ -0,0 +1,14 @@
+list1 = [10, 20, 30, 40]
+list2 = [50, 60, 70, 80]
+zippedList = zip(list1, list2)
+
+for zItem in zippedList:
+    print('lis1={}, lis2={}'.format(zItem[0], zItem[1]))
+
+items = ['apple', 'orange', 'mango', 'chocolate', 'cake', ]
+quantityInKg = [2, 3, 2.5, 1, 1]
+price = [345, 450, 100, 400, 250]
+
+zipped = zip(items, quantityInKg, price)
+for zippedItem in zipped:
+    print(zippedItem[2], ' rupees for ', zippedItem[1], ' KG of ', zippedItem[0])
