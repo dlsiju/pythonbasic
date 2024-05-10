aa86f13d408f1675bb09c25e9800676a79bcbe55
Siju Xminds <sijudl@xminds.com>
2024-05-10 13:54:18 +0530

    Write a program to return a set of elements present in set1 or set2, but not both

---
+++ CombineUniqElementsFromSet.py
@@ -0,0 +1,12 @@
+
+#2.Write a program to return a set of elements present in set1 or set2, but not both
+
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print('set1 =', set1)
+print('set2 =', set2)
+diff=set1.union(set2)-set1.intersection(set2)
+print('set of elements from both list but not common elements=',diff)
+
+
