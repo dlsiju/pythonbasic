2eed61509d712fba12a1d6c77f93c5093584152b
Siju Xminds <sijudl@xminds.com>
2024-05-10 15:33:08 +0530

    Write a Python program to return a new set with unique items from both sets by removing duplicates

---
+++ RetunNewSetWithUniqueItems.py
@@ -0,0 +1,7 @@
+# Write a Python program to return a new set with unique items from both sets by removing duplicates
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+union = set1.union(set2)
+print("all elements from both sets are: ", union)
