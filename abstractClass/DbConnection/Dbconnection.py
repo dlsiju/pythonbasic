4c331edd7d8cc2c3b1795577cee57745cac0fd5b
Siju Xminds <sijudl@xminds.com>
2024-05-16 13:38:12 +0530

    Abstarct class db connection

--- abstractClass/DbConnection/Dbconnection.py
+++ abstractClass/DbConnection/Dbconnection.py
@@ -1,10 +1,12 @@
-from abc import ABC
+from abc import abstractmethod, ABC
 
 
 class DbConnection(ABC):
 
+    @abstractmethod
     def connectToDb(self):
         pass
 
+    @abstractmethod
     def disconnect(self):
         pass
